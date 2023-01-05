48e432c8f0d061525b3721a37ebb053474237637
apoorvnv <104858693+apoorvnv@users.noreply.github.com>
2023-01-05 22:52:15 +0530

    Create otp_p1.py

---
+++ ContinuousAssessment/otp_p1.py
@@ -0,0 +1,26 @@
+# SEND MAIL FROM SENDER TO RECEIVER MAIL
+
+import random
+import smtplib  #number of digits of OTP
+
+def get_email():
+    n=int(input("Enter value of number of digits to generate OTP: "))
+def GENERATE_OTP():
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('apoorvnv@gmail.com',password='suua ormr pojn hxrm')
+    otp=''.join([str(random.randint(0,9))for i in range(n)])
+    msg='Hello, your otp is '+str(otp)
+    server.sendmail('apoorvnv@gmail.com','anyscientist2002@gmail.com',msg)
+    # server.sendmail('apoorvnv@gmail.com','ashwindg2020@gmail.com',msg)
+    # server.sendmail('apoorvnv@gmail.com', 'hardikachaudhari02@gmail.com', msg)
+    server.quit()
+    print(otp)
+get_email()
+GENERATE_OTP()
+
+''' MAKE FOLLOWING CHANGES IN CODE'''
+# 1.minimum 2 function (done)
+# 2.variables length (done)
+# 3.Try to avoid HARDCODING
+# mail this code at -->> " awk@dbatu.ac.in " <<--
